5d73c250ddb5f32e22636aa02b9faf4bf7cb3854
Bruno Gomes <brunogomes1198@gmail.com>
2023-03-09 20:44:43 +1100

    Create login and input components and apply Ternary Operator & AND Operator

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,16 @@
 import React from "react";
+import Login from "./Login";
+
+var isLoggedIn = false;
+
+const currentTime = new Date(2019, 11, 1, 9).getHours()
 
 function App() {
   return (
-    <div className="container">
-      <h1>Hello</h1>
-      <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
+    <div className="container"> {
+        //isLoggedIn ? <h1>Hello</h1> : <Login />
+        currentTime > 12 && <h1>Why are you still working?</h1>
+      } 
     </div>
   );
 }
